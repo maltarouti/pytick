eb75e5eec4fd1671f53526353d5708430d3b1cab
Murtada Altarouti <63660298+Murtada-Altarouti@users.noreply.github.com>
2023-09-07 10:04:30 +0300

    first commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='pytick',
+    version='1.0.0',
+    author='Murtada Altarouti',
+    description=('pytick is a lightweight Python library that provides a ',
+                 'simple and intuitive timer class for accurate time ',
+                 'interval measurements. It allows you to track elapsed time',
+                 'account for waiting periods, and calculate net time. Ideal',
+                 'for benchmarking, performance analysis, and duration ',
+                 'tracking in Python applications.'),
+    packages=[''],
+)
