5e63f1768c81dfd8208ccae14a8bac19242345e7
Murtada Altarouti <63660298+Murtada-Altarouti@users.noreply.github.com>
2023-09-07 10:25:32 +0300

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,10 @@ from setuptools import setup
 
 setup(
     name='pytick',
-    version='1.0.1',
+    version='1.0.0',
     author='Murtada Altarouti',
     description='pytick is a lightweight Python library that provides a simple and intuitive timer class for accurate time interval measurements. It allows you to track elapsed time account for waiting periods, and calculate net time. Ideal for benchmarking, performance analysis, and duration tracking in Python applications',  # noqa
     packages=[''],
+    license='MIT',
+    url='https://github.com/maltarouti/pytick',
 )
